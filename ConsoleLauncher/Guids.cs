782b9ef185763500f3297a2b83faf6e279898d61
Grzegorz Kozub <grzegorz.kozub@gmail.com>
2017-03-23 12:54:51 +0100

    Fix ConsoleLauncher GUIDs

--- ConsoleLauncher/Guids.cs
+++ ConsoleLauncher/Guids.cs
@@ -8,7 +8,7 @@ namespace GrzegorzKozub.VisualStudioExtensions.ConsoleLauncher
 {
     internal static class Guids
     {
-        internal const string Package = "83515e4e-9bd6-4d61-a95d-0a5b2bd5c518";
+        internal const string Package = "3d43bce4-95fc-4479-a0d5-8badbcde1a9f";
         internal const string Options = "bf2826ca-8255-4368-8927-bb0c21a91aca";
         internal static readonly Guid MenuGroup = new Guid("b3545520-bd53-499b-831d-5f33baf3ba6d");
     }
