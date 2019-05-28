09da0da0090d73363696d1350700dc1225a4af8e
Grzegorz Kozub <grzegorz.kozub@gmail.com>
2019-05-28 19:05:48 +0200

    Fix version bump

--- ConEmuLauncher/ConEmuLauncherPackage.cs
+++ ConEmuLauncher/ConEmuLauncherPackage.cs
@@ -13,7 +13,7 @@ using EnvDTE;
 namespace GrzegorzKozub.VisualStudioExtensions.ConEmuLauncher
 {
     [Guid(Guids.Package)]
-    [InstalledProductRegistration("#1", "#2", "1.2.0.0", IconResourceID = 3)]
+    [InstalledProductRegistration("#1", "#2", "1.3.0.0", IconResourceID = 3)]
     [PackageRegistration(UseManagedResourcesOnly = true)]
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [ProvideOptionPage(typeof(Options), "ConEmu Launcher", "General", 0, 0, false)]
